4892ddabe3948f16a898c641bd3fadb1ce269387
hjacquin <hj@taktik.com>
2019-04-26 17:43:36 +0200

    Fix unit test: replace dummy.restapiexample with SinonFakeServer

--- test/test-unitaires-execute-server.ts
+++ test/test-unitaires-execute-server.ts
@@ -4,91 +4,158 @@ import { httpclient } from '../src/index'
 import sinon, { SinonFakeServer } from 'sinon'
 
 describe('execute', function() {
-	let aRequest
+	let server: SinonFakeServer
 	describe('execute with a real API external', function() {
-		let postResponse: Object
-		let idOfCreatedObject: number
-		before(async function() {
-			this.timeout(5000)
-			aRequest = new httpclient.Request('http://dummy.restapiexample.com/api/v1/create')
+		before(function() {
+			server = sinon.fakeServer.create()
+		})
+		after(function() {
+			server.restore()
+		})
+
+		it('should be able to create an entry', async function() {
+			const aRequest = new httpclient.Request('http://dummy.restapiexample.com/api/v1/create')
 			aRequest.method = 'POST'
 			aRequest.body = {
 				'name': 'auDD',
 				'salary': '333000',
 				'age': '22'
 			}
-			postResponse = await execute(aRequest)
-		})
-		it('should be able to create an entry', function() {
-			assert.isAbove((postResponse as any).status, 199)
-			assert.isBelow((postResponse as any).status, 300)
+			server.respondWith(
+				'POST',
+				'http://dummy.restapiexample.com/api/v1/create',
+				[
+					200,
+					{ 'Content-Type': 'application/json' },
+					JSON.stringify({ id: 'anID' })
+				]
+			)
+			const postResponsePromises = execute(aRequest)
+			server.respond()
+			const postResponse = await postResponsePromises
+			assert.equal((postResponse as any).status, 200)
 		})
 		it('should be able to find the entry we posted before', async function() {
-			this.timeout(5000)
-			aRequest = new httpclient.Request('http://dummy.restapiexample.com/api/v1/employees')
-			const getResponse = await execute(aRequest)
-			let allEmployees = getResponse.body
-			let theEmployee = (allEmployees as Array<Object>).find((k) => (k as any).employee_name === 'auDD')
-			idOfCreatedObject = (theEmployee as any).id
-			assert.equal((theEmployee as any).employee_salary,333000)
-			assert.equal((theEmployee as any).employee_age,22)
+
+			server.respondWith(
+				'GET',
+				'http://dummy.restapiexample.com/api/v1/employees',
+				[
+					200,
+					{ 'Content-Type': 'application/json' },
+					JSON.stringify([ {
+						'name': 'auDD',
+						'salary': '333000',
+						'age': '22'
+					}])
+				]
+			)
+
+			const aRequest = new httpclient.Request('http://dummy.restapiexample.com/api/v1/employees')
+			const postResponsePromises = execute<any[]>(aRequest)
+			server.respond()
+			const getResponse = await postResponsePromises
+			const employees = getResponse.body
+			assert.deepEqual(employees, [ {
+				'name': 'auDD',
+				'salary': '333000',
+				'age': '22'
+			}])
 		})
 		it('should be able to modify the entry we added', async function() {
-			this.timeout(5000)
-			aRequest = new httpclient.Request('http://dummy.restapiexample.com/api/v1/update/' + idOfCreatedObject)
+			const expectedResponse = [ {
+				'name': 'auDD',
+				'salary': '333000',
+				'age': '22'
+			}]
+			server.respondWith(
+				'PUT',
+				'http://dummy.restapiexample.com/api/v1/update/anID',
+				[
+					200,
+					{ 'Content-Type': 'application/json' },
+					JSON.stringify(expectedResponse)
+				]
+			)
+			const aRequest = new httpclient.Request('http://dummy.restapiexample.com/api/v1/update/anID')
 			aRequest.method = 'PUT'
 			aRequest.body = {
 				'name': 'pnl',
 				'salary': '1000000',
 				'age': '22'
 			}
-			let putResponse = await execute(aRequest)
-			aRequest = new httpclient.Request('http://dummy.restapiexample.com/api/v1/employees')
-			const getResponse = await execute(aRequest)
-			let allEmployees = getResponse.body
-			let theEmployee = (allEmployees as Array<Object>).find((k) => (k as any).employee_name === 'pnl')
-			assert.equal(idOfCreatedObject,(theEmployee as any).id)
-			assert.equal((theEmployee as any).employee_salary,1000000)
-			assert.equal((theEmployee as any).employee_age,22)
+			const postResponsePromises = execute(aRequest)
+			server.respond()
+			const putResponse = await postResponsePromises
+			assert.deepEqual(putResponse.body, expectedResponse)
+
 		})
 		it('should be able to delete the entry we modified before', async function() {
-			this.timeout(5000)
-			aRequest = new httpclient.Request('http://dummy.restapiexample.com/api/v1/delete/' + idOfCreatedObject)
+			server.respondWith(
+				'DELETE',
+				'http://dummy.restapiexample.com/api/v1/delete/idOfCreatedObject',
+				[
+					200,
+					{ 'Content-Type': 'application/json' },
+					JSON.stringify({ expectedResponse: 'any' })
+				]
+			)
+			const aRequest = new httpclient.Request('http://dummy.restapiexample.com/api/v1/delete/idOfCreatedObject')
 			aRequest.method = 'DELETE'
-			await execute(aRequest)
+			const postResponsePromises = execute(aRequest)
+			server.respond()
+			const response = await postResponsePromises
 			//
-			aRequest = new httpclient.Request('http://dummy.restapiexample.com/api/v1/employees')
-			const getResponse = await execute(aRequest)
-			let allEmployees = getResponse.body
-			let theEmployee = (allEmployees as Array<Object>).find((k) => (k as any).id === idOfCreatedObject)
-			assert.notExists(theEmployee)
+
+			assert.deepEqual(response.body, { expectedResponse: 'any' })
+
 		})
 		it('should throw an error if http request sent to bad url', async function() {
-			this.timeout(5000)
 			try {
-				aRequest = new httpclient.Request('i@#ccidently_mizspeld976theurl/')
-				await execute(aRequest)
+				server.respondWith(
+					'GET',
+					'http://dummy.restapiexample.com/i@#ccidently_mizspeld976theurl/',
+					[
+						404,
+						{ 'Content-Type': 'application/json' },
+						'not found'
+					]
+				)
+				const aRequest = new httpclient.Request('http://dummy.restapiexample.com/i@#ccidently_mizspeld976theurl/')
+				const responsePromise = execute(aRequest)
+				server.respond()
+				const response = await responsePromise
 				assert.isTrue(false, 'previous line should throw an error')
 			} catch (error) {
 				const response: httpclient.Response<Object> = error
 				assert.equal(response.status, 404)
 			}
 		})
-		it('should also throw an error if the routing does not support the verb', async function() {
-			this.timeout(5000)
+		it('should also throw an error on server error', async function() {
 			try {
-				aRequest = new httpclient.Request('http://dummy.restapiexample.com/api/v1/employees')
+				server.respondWith(
+					'PUT',
+					'http://dummy.restapiexample.com/api/v1/employees',
+					[
+						500,
+						{ 'Content-Type': 'application/json' },
+						'server error'
+					]
+				)
+				const aRequest = new httpclient.Request('http://dummy.restapiexample.com/api/v1/employees')
 				aRequest.method = 'PUT'
 				aRequest.body = {
 					'name': 'pnl',
 					'salary': '1000000',
 					'age': '22'
 				}
-				await execute(aRequest)
+				const responsePromise = execute(aRequest)
+				server.respond()
+				const response = await responsePromise
 				assert.isTrue(false, 'previous line should throw an error')
 			} catch (error) {
 				const response: httpclient.Response<Object> = error
-				assert.equal(response.status,405)
+				assert.equal(response.status,500)
 			}
 		})
 	})
